9d1a57d1a783f34b8ccedad37ab819a693e8f289
sintaafria <sintaafria16@gmail.com>
2021-10-31 18:45:47 +0700

    add reset button

--- src/Components/CalculatorComp/CalComp.jsx
+++ src/Components/CalculatorComp/CalComp.jsx
@@ -9,27 +9,39 @@ class CalComp extends React.Component {
     };
 
     handleSum = () => {
+        let a = Number(this.state.input1);
+        let b = Number(this.state.input2);
+        
         if (this.props.operator === '+'){
-            this.setState({output: this.state.input1 + this.state.input2})
+            this.setState({output: a + b})
         }
         if (this.props.operator === '-'){
-            this.setState({output: this.state.input1 - this.state.input2})
+            this.setState({output: a - b})
         }
         if (this.props.operator === 'x'){
-            this.setState({output: this.state.input1 * this.state.input2})
+            this.setState({output: a * b})
         }
         if (this.props.operator === '/'){
-            this.setState({output: this.state.input1 / this.state.input2})
+            this.setState({output: a / b})
         }
     }; 
+
+    handeReset = () => {
+        this.setState({
+            input1: '',
+            input2: '',
+            output: '',
+        })
+    }
     render() {
         return(
             <div>
-                <input type="text" value={this.state.input1} onChange={e =>this.setState({input1: Number(e.target.value)})}/>
+                <input type="text" value={this.state.input1} onChange={e =>this.setState({input1: e.target.value})}/>
                 <span><b> {this.props.operator} </b></span>
-                <input type="text" value={this.state.input2} onChange={e =>this.setState({input2: Number(e.target.value)})}/>
+                <input type="text" value={this.state.input2} onChange={e =>this.setState({input2: e.target.value})}/>
                 <span>  </span><button onClick={this.handleSum}><b> = </b></button> <span>  </span>
-                <input type="text" defaultValue={this.state.output}/>
+                <input type="text" defaultValue={this.state.output}/><br/>
+                <button onClick={this.handeReset}>Reset</button>
             </div>
         )
     }
